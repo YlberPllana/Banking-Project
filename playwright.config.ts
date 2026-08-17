4bcd5cb8aa30287b554d158f21fb42167f902a9e
Ylber Pllana <ylberhpllana@gmail.com>
2026-08-17 17:23:05 +0200

    added trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
